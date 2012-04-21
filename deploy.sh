0bcc2fde12ad9a6984b6ed71525d55933faba303
Paolo G. Lim <paolo@caresharing.eu>
2012-04-21 23:03:09 +0800

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+sudo apt-get update && sudo apt-get upgrade
+
+# base packages
+sudo apt-get install build-essential ubuntu-restricted-extras openjdk-7-jre
+
+# utilities
+sudo apt-get install terminator parcellite keepassx emacs vim kupfer wine xmonad curl
+
+# media
+sudo apt-get install vlc rhythmbox flashplugin-nonfree
+
+# internet
+sudo apt-get install pidgin skype chromium-browser transmission thunderbird
+
+# git
+sudo apt-get install git-core tig
+
+# rails
+curl -L get.rvm.io | bash -s stable
+
+sudo apt-get install bison openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev
+
+# mysql
+
+# others
